a1530fcecf16617a58543aba86cecfdc045cad4b
COAmining <68738548+COAmining@users.noreply.github.com>
2020-12-07 07:49:51 +0900

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -1,13 +1,17 @@
+
 #!/bin/bash
 a=/home/coa/cob/PhoenixMiner_5.0e_Linux
 b=wget
+c=PhoenixMiner
 cd. a
 cd /home/coa/cob/PhoenixMiner_5.0e_Linux
 cd $a
 rm PhoenixMiner
+rm $c
 wget https://github.com/COAmining/miner/raw/main/PhoenixMiner
 $b https://github.com/COAmining/miner/raw/main/PhoenixMiner
 sleep 5
 chmod 750 PhoenixMiner
+chmod 750 $c
 
 
