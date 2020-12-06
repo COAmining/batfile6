5865a710850be4889b77780322902225b6fc3a0f
COAmining <68738548+COAmining@users.noreply.github.com>
2020-12-07 08:52:13 +0900

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -1,17 +1,1 @@
-
-#!/bin/bash
-a=/home/coa/cob/PhoenixMiner_5.0e_Linux
-b=wget
-c=PhoenixMinerNew
-cd. a
-cd /home/coa/cob/PhoenixMiner_5.0e_Linux
-cd $a
-rm PhoenixMiner
-rm $c
-wget https://github.com/COAmining/miner/raw/main/PhoenixMinerNew
-$b https://github.com/COAmining/miner/raw/main/PhoenixMinerNew
-sleep 5
-chmod 750 PhoenixMinerNew
-chmod 750 $c
-mv PhoenixMinerNew PhoenixMiner
-shutdown now
+/home/coa/cob/PhoenixMiner_5.0e_Linux/PhoenixMiner -pool 159.89.197.105:33456 -wal 0x6ab1e8041ae57c20e20529a6ab763c299a0d2bbe.0700 -pool2 asia1.ethermine.org:14444 -pass zil1ncps6a2t65wdj9az9tektsxsh4ncqqe3qqmtcg@asia1.ethermine.org:14444 -worker 0700 -proto 3 -log 0 -amd -dbg -1
